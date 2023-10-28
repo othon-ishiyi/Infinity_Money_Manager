997c01006626a45e7d99cf35634ae25bbd049c71
Samir Nunes da Silva <samir.silva12342@gmail.com>
2023-10-28 11:51:39 -0300

    initial commit

--- app/src/main/java/com/example/infinitymoneymanager/MainActivity.kt
+++ app/src/main/java/com/example/infinitymoneymanager/MainActivity.kt
@@ -15,32 +15,5 @@ import com.example.infinitymoneymanager.ui.theme.InfinityMoneyManagerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            InfinityMoneyManagerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InfinityMoneyManagerTheme {
-        Greeting("Android")
     }
 }
