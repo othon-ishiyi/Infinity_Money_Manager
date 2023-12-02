b0531996c9e186a615cbcaf90f658fa22fe625cc
othon-ishiyi <othon.ishiyi@gmail.com>
2023-12-02 20:33:45 -0300

    added colors

--- app/src/main/java/com/example/infinitymoneymanager/InfinityScreen.kt
+++ app/src/main/java/com/example/infinitymoneymanager/InfinityScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.material.icons.filled.TrendingUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.Color
@@ -29,9 +31,18 @@ import com.example.infinitymoneymanager.ui.GoalScreen
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
+//Defining the screens for navigation
 sealed class BottomNavItem(var title:String, var icon:ImageVector, var screen_route:String){
     object Composition : BottomNavItem("Composition", Icons.Filled.PieChart,"composition")
     object Evolution: BottomNavItem("Evolution", Icons.Filled.TrendingUp,"evolution")
@@ -39,11 +50,26 @@ sealed class BottomNavItem(var title:String, var icon:ImageVector, var screen_ro
 }
 
 @Composable
-fun InfinityApp(
-    navController: NavHostController = rememberNavController(),
+fun InfinityAppBar(
     modifier: Modifier = Modifier
 ) {
+    TopAppBar(
+        title = {
+            Text(stringResource(R.string.app_name))},
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier
+    )
+}
+
+// Main app
+@Composable
+fun InfinityApp(
+    navController: NavHostController = rememberNavController(),
+) {
     Scaffold (
+        topBar = { InfinityAppBar() },
         bottomBar = {BottomNavigation(navController = navController)}
     ) { innerPadding ->
         NavHost(
@@ -71,15 +97,31 @@ fun BottomNavigation(navController: NavController) {
         BottomNavItem.Evolution,
         BottomNavItem.Goal,
     )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
     NavigationBar(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
         modifier = Modifier
     ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
+
         items.forEach { item ->
+
+            val iconColor = if (currentRoute == item.screen_route) MaterialTheme.colorScheme.onPrimary
+            else MaterialTheme.colorScheme.onPrimaryContainer
+
             NavigationBarItem(
-                icon = { Icon(imageVector = item.icon, contentDescription = item.title) },
-                label = { Text(text = item.title)},
+                icon = {
+                    Icon(
+                        imageVector = item.icon,
+                        contentDescription = item.title,
+                        tint = iconColor
+                    )},
+                label = {
+                    Text(
+                        text = item.title,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )},
                 selected = (currentRoute == item.screen_route),
                 onClick = {
                     navController.navigate(item.screen_route) {
--- app/src/main/java/com/example/infinitymoneymanager/MainActivity.kt
+++ app/src/main/java/com/example/infinitymoneymanager/MainActivity.kt
@@ -3,13 +3,9 @@ package com.example.infinitymoneymanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import androidx.fragment.app.Fragment
-import com.example.infinitymoneymanager.databinding.ActivityMainBinding
 import com.example.infinitymoneymanager.ui.theme.InfinityMoneyManagerTheme
 
 class MainActivity : ComponentActivity() {
